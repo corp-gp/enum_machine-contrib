d5595bcde0da6571a10cb9c268152a4a9d5f9b3a
Sergei Malykh <xronos.i.am@gmail.com>
2023-05-11 19:47:27 +0300

    wip

--- lib/enum_machine_contrib/decision_graph.rb
+++ lib/enum_machine_contrib/decision_graph.rb
@@ -97,10 +97,19 @@ module EnumMachineContrib
       output_values = component_cycled_vertex.outcoming_edges.flat_map { |vertex| vertex.to.value }
 
       active_vertexes = vertexes.filter(&:active?)
-      input_vertexes = active_vertexes.reject { |vertex| (input_values & vertex.value).empty? }
+      input_vertexes  = active_vertexes.reject { |vertex| (input_values & vertex.value).empty? }
       output_vertexes = active_vertexes.reject { |vertex| (output_values & vertex.value).empty? }
 
       component_vertexes = active_vertexes.reject { |vertex| (component_cycled_vertex.value & vertex.value).empty? }
+
+      component_vertexes.each do |vertex|
+        vertex.incoming_edges.each do |edge|
+          if (input_vertexes + component_vertexes).exclude?(edge.from)
+            # drop insignificant incoming edges
+            edge.dropped!
+          end
+        end
+      end
 
       single_incoming_vertexes = (component_vertexes + output_vertexes).filter { |vertex| vertex.incoming_edges.size == 1 }
 
